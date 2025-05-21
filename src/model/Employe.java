d93ae3d4e0a5964df41ce02044496c2804a0633d
MOUSLIM11 <161123105+MOUSLIM11@users.noreply.github.com>
2025-05-21 14:03:45 +0100

    Update Employe.java

--- src/model/Employe.java
+++ src/model/Employe.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Employe extends Personne {
     private final String matricule;
     private final Fonction fonction;
-
+//constructor
     public Employe(String matricule, Fonction fonction, String nom, String prenom, LocalDate dateNaissance, boolean handicap) {
         super(nom, prenom, dateNaissance, handicap);
         this.matricule = matricule;
